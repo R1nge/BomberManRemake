f8dd5b347e305c2004e1903cedbb9c0e68041325
R1nge <r1ngegamedev@gmail.com>
2023-09-03 16:50:31 +0300

    Lobby (WIP)

--- Assets/Scripts/Lobby/PlayerModel.cs
+++ Assets/Scripts/Lobby/PlayerModel.cs
@@ -1,41 +1,22 @@
 ﻿using TMPro;
 using Unity.Netcode;
 using UnityEngine;
-using Zenject;
 
 namespace Lobby
 {
     public class PlayerModel : NetworkBehaviour
     {
         [SerializeField] private TextMeshProUGUI ready;
-        private Lobby _lobby;
-
-        [Inject]
-        private void Inject(Lobby lobby)
-        {
-            _lobby = lobby;
-        }
-
-        public override void OnNetworkSpawn()
-        {
-            if (IsServer)
-            {
-                _lobby.OnReadyStateChanged += UpdateUIServerRpc;
-            }
-        }
 
         [ServerRpc(RequireOwnership = false)]
-        private void UpdateUIServerRpc(ulong clientId, bool isReady)
+        public void UpdateUIServerRpc(bool isReady)
         {
-            ready.text = isReady.ToString();
-            print("SERVER UPDATE READY UI");
-            UpdateUIClientRpc(clientId, isReady);
+            UpdateUIClientRpc(isReady);
         }
 
         [ClientRpc]
-        private void UpdateUIClientRpc(ulong clientId, bool isReady)
+        private void UpdateUIClientRpc(bool isReady)
         {
-            if(IsServer) return;
             ready.text = isReady.ToString();
             print("UPDATE READY UI");
         }
