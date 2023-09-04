8f6ca460b0b9ab916d999e8d4fe24b16fd185c17
R1nge <r1ngegamedev@gmail.com>
2023-09-04 15:37:43 +0300

    Spawn player in corners of the map

--- Assets/Scripts/Game/PlayerSpawner.cs
+++ Assets/Scripts/Game/PlayerSpawner.cs
@@ -1,24 +1,108 @@
-﻿using Unity.Netcode;
+﻿using System;
+using Unity.Netcode;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Game
 {
     public class PlayerSpawner : NetworkBehaviour
     {
+        [SerializeField] private MapPreset mapPreset;
         [SerializeField] private GameObject playerPrefab;
+        private NetworkVariable<bool> _leftTop, _rightTop, _rightBottom, _leftBottom;
         private DiContainer _diContainer;
+        private MapSettings _mapSettings;
+
+        private void Awake()
+        {
+            _leftTop = new NetworkVariable<bool>();
+            _rightTop = new NetworkVariable<bool>();
+            _rightBottom = new NetworkVariable<bool>();
+            _leftBottom = new NetworkVariable<bool>();
+        }
 
         [Inject]
-        private void Inject(DiContainer diContainer) => _diContainer = diContainer;
+        private void Inject(DiContainer diContainer, MapSettings mapSettings)
+        {
+            _diContainer = diContainer;
+            _mapSettings = mapSettings;
+        }
 
         public override void OnNetworkSpawn() => SpawnServerRpc();
 
         [ServerRpc(RequireOwnership = false)]
         private void SpawnServerRpc(ServerRpcParams rpcParams = default)
         {
-            var player = _diContainer.InstantiatePrefab(playerPrefab);
+            var player = _diContainer.InstantiatePrefab(playerPrefab, PickPosition(), Quaternion.identity, null);
             player.GetComponent<NetworkObject>().SpawnWithOwnership(rpcParams.Receive.SenderClientId);
+        }
+
+        private Vector3 PickPosition()
+        {
+            var position = Random.Range(0, 4);
+            if (position == 0)
+            {
+                if (!_leftBottom.Value)
+                {
+                    return LeftBottomCorner();
+                }
+            }
+            else if (position == 1)
+            {
+                if (!_leftTop.Value)
+                {
+                    return LeftTopCorner();
+                }
+            }
+            else if (position == 2)
+            {
+                if (!_rightTop.Value)
+                {
+                    return RightTopCorner();
+                }
+            }
+            else if (position == 3)
+            {
+                if (!_rightBottom.Value)
+                {
+                    return RightBottomCorner();
+                }
+            }
+            else
+            {
+                return Vector3.zero;
+            }
+
+            return PickPosition();
+        }
+
+        private Vector3 LeftTopCorner()
+        {
+            _leftTop.Value = true;
+            print("Left Top");
+            return new Vector3(0, 0, (_mapSettings.Length - 1) * mapPreset.Size);
+        }
+
+        private Vector3 RightTopCorner()
+        {
+            _rightTop.Value = true;
+            print("Right Top");
+            return new Vector3((_mapSettings.Width - 1) * mapPreset.Size, 0, (_mapSettings.Length - 1) * mapPreset.Size);
+        }
+
+        private Vector3 RightBottomCorner()
+        {
+            _rightBottom.Value = true;
+            print("Right Bottom");
+            return new Vector3(0, 0, (_mapSettings.Length - 1) * mapPreset.Size);
+        }
+
+        private Vector3 LeftBottomCorner()
+        {
+            _leftBottom.Value = true;
+            print("Left Bottom");
+            return new Vector3(0, 0, 0);
         }
     }
 }
