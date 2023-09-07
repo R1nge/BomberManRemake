452332588ea4ac5a36bc196a4b90badefc6023a9
R1nge <r1ngegamedev@gmail.com>
2023-09-07 21:59:34 +0300

    Render only shadow locally FPS character

---
+++ Assets/Scripts/Misc/RenderOnlyShadowLocally.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Misc
+{
+    public class RenderOnlyShadowLocally : NetworkBehaviour
+    {
+        [SerializeField] private new Renderer renderer;
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner)
+            {
+                renderer.shadowCastingMode = ShadowCastingMode.ShadowsOnly;
+            }
+        }
+    }
+}
