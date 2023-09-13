d58755fe7ee5808446ac34079fec815ad1297483
R1nge <r1ngegamedev@gmail.com>
2023-09-13 15:08:37 +0300

    Audio settings

---
+++ Assets/Scripts/Settings/AudioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Settings
+{
+    public class AudioController : MonoBehaviour
+    {
+        public event Action<float, float, float> OnSettingsLoaded;
+        [SerializeField] private AudioMixer audioMixer;
+        private const string MASTER_VOLUME = "MasterVolume";
+        private const string MUSIC_VOLUME = "MusicVolume";
+        private const string SFX_VOLUME = "SfxVolume";
+
+        private void Awake() => Load();
+
+        public void SetMasterVolume(float volume) => audioMixer.SetFloat(MASTER_VOLUME, volume);
+
+        public void SetMusicVolume(float volume) => audioMixer.SetFloat(MUSIC_VOLUME, volume);
+
+        public void SetSfxVolume(float volume) => audioMixer.SetFloat(SFX_VOLUME, volume);
+
+        private void Load()
+        {
+            var masterVolume = PlayerPrefs.GetFloat(MASTER_VOLUME, -50f);
+            var musicVolume = PlayerPrefs.GetFloat(MUSIC_VOLUME, -50f);
+            var sfxVolume = PlayerPrefs.GetFloat(SFX_VOLUME, -50f);
+            OnSettingsLoaded?.Invoke(masterVolume, musicVolume, sfxVolume);
+        }
+
+        private void OnApplicationQuit() => Save();
+
+        private void Save()
+        {
+            audioMixer.GetFloat(MASTER_VOLUME, out var masterVolume);
+            PlayerPrefs.SetFloat(MASTER_VOLUME, masterVolume);
+            audioMixer.GetFloat(MUSIC_VOLUME, out var musicVolume);
+            PlayerPrefs.SetFloat(MUSIC_VOLUME, musicVolume);
+            audioMixer.GetFloat(SFX_VOLUME, out var sfxVolume);
+            PlayerPrefs.SetFloat(SFX_VOLUME, sfxVolume);
+            PlayerPrefs.Save();
+        }
+    }
+}
---
+++ Assets/Scripts/Settings/AudioSettingsUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Settings
+{
+    public class AudioSettingsUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject UI;
+        [SerializeField] private Slider master, music, sfx;
+        [SerializeField] private Button open, close;
+        private AudioController _audioController;
+
+        private void Awake()
+        {
+            _audioController = GetComponent<AudioController>();
+            _audioController.OnSettingsLoaded += SettingsLoaded;
+            master.onValueChanged.AddListener(volume => _audioController.SetMasterVolume(volume));
+            music.onValueChanged.AddListener(volume => _audioController.SetMusicVolume(volume));
+            sfx.onValueChanged.AddListener(volume => _audioController.SetSfxVolume(volume));
+            open.onClick.AddListener(Open);
+            close.onClick.AddListener(Close);
+        }
+
+        private void Open() => UI.SetActive(true);
+
+        private void Close() => UI.SetActive(false);
+
+        private void SettingsLoaded(float masterValue, float musicValue, float sfxValue)
+        {
+            master.value = masterValue;
+            music.value = musicValue;
+            sfx.value = sfxValue;
+        }
+
+        private void OnDestroy()
+        {
+            master.onValueChanged.RemoveAllListeners();
+            music.onValueChanged.RemoveAllListeners();
+            sfx.onValueChanged.RemoveAllListeners();
+            open.onClick.RemoveAllListeners();
+            close.onClick.RemoveAllListeners();
+        }
+    }
+}
