f4faa70aee8bde67fb96c38c819d87c9816a002c
R1nge <r1ngegamedev@gmail.com>
2023-09-14 15:01:53 +0300

    Ping UI

---
+++ Assets/Scripts/Misc/PingUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Misc
+{
+    public class PingUI : NetworkBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI ping;
+
+        private void Awake()
+        {
+            NetworkManager.Singleton.NetworkTickSystem.Tick += UpdateUI;
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            ping.gameObject.SetActive(IsOwner);
+        }
+
+        private void UpdateUI()
+        {
+            if (!IsOwner) return;
+            var pin = NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetCurrentRtt(NetworkManager.Singleton.NetworkConfig.NetworkTransport.ServerClientId);
+            pin /= 2;
+            ping.text = $"{pin} ms";
+        }
+    }
+}
