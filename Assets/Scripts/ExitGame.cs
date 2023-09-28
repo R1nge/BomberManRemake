98f7129c902215705ad956302f02201ec9d4a8f1
R1nge <r1ngegamedev@gmail.com>
2023-09-28 18:48:35 +0300

    Save before exiting the game

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class ExitGame
+{
+    public event Action OnGameExit;
+    private static bool _canExit;
+
+    public void Exit()
+    {
+        OnGameExit?.Invoke();
+        _canExit = true;
+    }
+    
+    [RuntimeInitializeOnLoadMethod]
+    static void RunOnStart()
+    {
+        Application.wantsToQuit += ApplicationOnwantsToQuit;
+    }
+
+    private static bool ApplicationOnwantsToQuit()
+    {
+        return _canExit;
+    }
+}
